ebc1abe09b43cdfbd851a087446e437460c50126
CitizenQuim <qpsilva89@gmail.com>
2025-02-03 15:22:07 +0000

    changes

---
+++ Ficha Trabalho 04/exercicio5.py
@@ -0,0 +1,4 @@
+# Loop que repete para os números de 1 a 100
+
+for i in range(1, 101):
+    print(i)
---
+++ Ficha Trabalho 04/exercicio6.py
@@ -0,0 +1,6 @@
+#Loop que percorre os números de 1 a 100
+
+for i in range(1, 101):
+    quadrado = i ** 2 #Calculando o quadrado do número
+    
+    print(f"Número:{i}, Quadrado: {quadrado}")
